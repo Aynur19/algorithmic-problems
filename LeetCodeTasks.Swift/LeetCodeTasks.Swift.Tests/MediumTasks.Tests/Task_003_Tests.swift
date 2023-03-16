1bc7a75769abb4c593ad20756b41af841eb1b328
Aynur19 <nasybullin.aynur@yandex.ru>
2023-03-16 20:47:15 +0300

    added solution and unit tests (swift) of task "3. Longest Substring Without Repeating Characters";

---
+++ LeetCodeTasks.Swift/LeetCodeTasks.Swift.Tests/MediumTasks.Tests/Task_003_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Task_003_Tests.swift
+//  LeetCodeTasks.Swift.Tests
+//
+//  Created by Aynur Nasybullin on 16.03.2023.
+//
+
+import XCTest
+@testable import LeetCodeTasks_Swift
+
+final class Task_003_Tests: XCTestCase {
+    var task: Task_003!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        task = Task_003()
+    }
+    
+    override func tearDownWithError() throws {
+        task = nil
+        try super.tearDownWithError()
+    }
+    
+    func test_lengthOfLongestSubstring() throws {
+        XCTAssertEqual(task.lengthOfLongestSubstring("abcabcbb"), 3)
+        XCTAssertEqual(task.lengthOfLongestSubstring("bbbbb"), 1)
+        XCTAssertEqual(task.lengthOfLongestSubstring("wwkew"), 3)
+        XCTAssertEqual(task.lengthOfLongestSubstring(" "), 1)
+        XCTAssertEqual(task.lengthOfLongestSubstring("dvdf"), 3)
+        XCTAssertEqual(task.lengthOfLongestSubstring("asjrgapa"), 6)
+    }
+}
