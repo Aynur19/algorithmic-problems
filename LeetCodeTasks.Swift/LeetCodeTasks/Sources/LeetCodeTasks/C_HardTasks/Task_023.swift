317928c086d8e5ba5a936d70780b2a21887cda61
Aynur19 <nasybullin.aynur@yandex.ru>
2023-03-26 10:00:29 +0300

    added solution and unit tests (swift) of task "23. Merge k Sorted Lists";

--- LeetCodeTasks.Swift/LeetCodeTasks/Sources/LeetCodeTasks/C_HardTasks/Task_023.swift
+++ LeetCodeTasks.Swift/LeetCodeTasks/Sources/LeetCodeTasks/C_HardTasks/Task_023.swift
@@ -5,7 +5,7 @@
 //  Created by Aynur Nasybullin on 25.03.2023.
 //
 
-/// 23. Merge k Sorted Lists
+/// 23. Merge k Sorted Lists:
 ///
 /// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 ///
@@ -20,53 +20,51 @@
 /// 5) lists[i] is sorted in ascending order.
 /// 5) The sum of lists[i].length will not exceed 10^4.
 
+//      1               4   5
+//  1           3   4
+//          2                   6
+
 
 import Foundation
 
 extension HardTasks {
-    private func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
-        guard list1 != nil, list2 != nil else {
-            if list1 == nil && list2 == nil { return nil }
-            else if list1 == nil { return list2 }
-            else { return list1 }
-        }
-        
-        var l1 = list1, l2 = list2
-        var result: ListNode? = ListNode()
-        var tmp: ListNode? = result
-                              
-        while l1 != nil && l2 != nil {
-            if l1!.val >= l2!.val {
-                tmp?.next = l2
-                tmp = tmp?.next
-                l2 = l2?.next
-            } else {
-                tmp?.next = l1
-                tmp = tmp?.next
-                l1 = l1?.next
-            }
-        }
-        
-        if l1 == nil {
-            tmp?.next = l2
-        } else {
-            tmp?.next = l1
-        }
-        
-        return result?.next
-    }
-    
     func mergeKLists(_ lists: [ListNode?]) -> ListNode? {
         guard !lists.isEmpty else { return nil }
         
-        if lists.count < 2 { return lists[0] }
-        
-        var result: ListNode?
-        
-        for i in 0..<lists.count {
-            result = mergeTwoLists(result, lists[i])
+        var nodesL: [ListNode?] = []
+        lists.forEach { ln in
+            if ln != nil {
+                nodesL.append(ln)
+            }
+        }
+        guard !nodesL.isEmpty else { return nil }
+
+        let result = ListNode()
+        var dict: [Int:[ListNode]] = [:]
+        var tmp: ListNode?
+        for i in 0..<nodesL.count {
+            tmp = nodesL[i]
+            
+            while tmp != nil {
+                if dict[tmp!.val] == nil {
+                    dict[tmp!.val] = [tmp!]
+                } else {
+                    dict[tmp!.val]!.append(tmp!)
+                }
+                tmp = tmp?.next
+            }
         }
         
-        return result
+        let keys = Array(dict.keys.sorted())
+        tmp = result
+        keys.forEach { k in
+            dict[k]?.forEach { ln in
+                tmp?.next = ln
+                tmp = tmp?.next
+            }
+        }
+        
+        
+        return result.next
     }
 }
