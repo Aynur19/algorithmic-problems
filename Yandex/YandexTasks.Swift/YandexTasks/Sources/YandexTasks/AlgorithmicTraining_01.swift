e466619a41f12a666ff8073c687d565fae2afe8e
Aynur19 <nasybullin.aynur@yandex.ru>
2023-03-29 22:02:51 +0300

    added solution and unit tests (swift) of Yandex Algorithmic Taining 1.0: Problem C";

--- Yandex/YandexTasks.Swift/YandexTasks/Sources/YandexTasks/AlgorithmicTraining_01.swift
+++ Yandex/YandexTasks.Swift/YandexTasks/Sources/YandexTasks/AlgorithmicTraining_01.swift
@@ -8,6 +8,34 @@
 import Foundation
 
 class AlgorithmicTraining_01 {
+    func problem_C(_ lines: [String]) -> [String] {
+        var phone: String = ""
+        var result: [String] = []
+        let droped: [String.Element] = ["-", "(", ")"]
+        var tmp: String = ""
+        
+        for i in 0..<lines.count {
+            tmp = lines[i]
+            tmp.removeAll(where: {
+                droped.contains(_: $0)
+            })
+            
+            if tmp.hasPrefix("+7") {
+                tmp = "8\(String(tmp.dropFirst(2)))"
+            } else if tmp.count < 11 {
+                tmp = "8495\(tmp)"
+            }
+            
+            if i > 0 {
+                phone == tmp ? result.append("YES") : result.append("NO")
+            } else {
+                phone = tmp
+            }
+        }
+        
+        return result
+    }
+    
     func problem_B(_ line1: String, _ line2: String, _ line3: String) -> String {
         let sides = [Int(line1)!, Int(line2)!, Int(line3)!].sorted()
         
@@ -27,11 +55,16 @@ class AlgorithmicTraining_01 {
     }
 }
 
-//let line1 = readLine()!
-//let line2 = readLine()!
-//let line3 = readLine()!
+//var lines: String[] = []
+//
+//for i in 0..<4 {
+//    lines.append(readLine()!)
+//}
 //
 //let solution = AlgorithmicTraining_01()
 //let result = solution.problem_B(line1, line2, line3)
 //
-//print(result)
+//for i in 0..<result.count {
+//    print(result[i])
+//}
+
--- Yandex/YandexTasks.Swift/YandexTasks/Tests/YandexTasksTests/AlgorithmicTraining_01_Tests.swift
+++ Yandex/YandexTasks.Swift/YandexTasks/Tests/YandexTasksTests/AlgorithmicTraining_01_Tests.swift
@@ -26,6 +26,15 @@ final class AlgorithmicTraining_01_Tests: XCTestCase {
 
 
 extension AlgorithmicTraining_01_Tests {
+    func test_problem_C() throws {
+        XCTAssertEqual(task.problem_C(["8(495)430-23-97", "+7-4-9-5-43-023-97", "4-3-0-2-3-9-7", "8-495-430"]),
+                       ["YES", "YES", "NO"])
+        XCTAssertEqual(task.problem_C(["86406361642", "83341994118", "86406361642", "83341994118"]),
+                       ["NO", "YES", "NO"])
+        XCTAssertEqual(task.problem_C(["+78047952807", "+78047952807", "+76147514928", "88047952807"]),
+                       ["YES", "NO", "YES"])
+    }
+    
     func test_problem_B() throws {
         XCTAssertEqual(task.problem_B("3", "4", "5"), "YES")
         XCTAssertEqual(task.problem_B("3", "5", "4"), "YES")
