93f5c01b443835f5aa2b7e86a0a6134ef42d9b8b
Aynur19 <nasybullin.aynur@yandex.ru>
2023-04-10 17:15:29 +0300

    added solution and unit tests (swift) of Yandex Algorithmic Taining 1.0 (3): Problem C";

--- Yandex/YandexTasks.Swift/YandexTasks/Sources/YandexTasks/AlgorithmicTraining_01_03.swift
+++ Yandex/YandexTasks.Swift/YandexTasks/Sources/YandexTasks/AlgorithmicTraining_01_03.swift
@@ -7,18 +7,39 @@
 
 import Foundation
 
+let nm = readLine()!.split(separator: " ").map({ Int($0)! })
+var a = Array<Int>(repeating: 0, count: nm[0])
+var b = Array<Int>(repeating: 0, count: nm[1])
+
+for i in 0..<a.count {
+    a[i] = Int(readLine()!)!
+}
+
+for i in 0..<b.count {
+    b[i] = Int(readLine()!)!
+}
+
+let solution = AlgorithmicTraining_01_03()
+let result = solution.problem_C(a, b)
+
+for i in 0..<result.count {
+    print(result[i].count)
+    print(result[i].map({ String($0)} ).joined(separator: " "))
+}
+
 
 class AlgorithmicTraining_01_03 {
-    func problem_C(_ nums1: String, _ nums2: String) -> [[Int]] {
-        let set1 = Set(nums1.split(separator: " ").map({ Int($0)! }))
-        let set2 = Set(nums2.split(separator: " ").map({ Int($0)! }))
-
+    func problem_C(_ nums1: [Int], _ nums2: [Int]) -> [[Int]] {
         var result: [[Int]] = Array(repeating: [], count: 3)
 
-        result[0] = set1.intersection(set2).sorted()
-        result[1] = set1.intersection(result[0]).sorted()
-        result[2] = set2.intersection(result[0]).sorted()
+        let set1 = Set(nums1)
+        let set2 = Set(nums2)
 
+        result[0] = set1.intersection(set2).sorted()
+        result[1] = set1.subtracting(result[0]).sorted()
+        result[2] = set2.subtracting(result[0]).sorted()
+
+        let a = result[0].map({ String($0)} ).joined(separator: " ")
         return result
     }
 
